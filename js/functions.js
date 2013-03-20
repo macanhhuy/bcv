3dd6fbbf0f378640784a6fada80cc7f979d3e6fa
macanhhuy <huymac@magrabbit.com>
2013-03-20 16:59:27 +0700

    New Design

--- js/functions.js
+++ js/functions.js
@@ -5,7 +5,7 @@ Cufon.replace('.status p', { fontFamily: 'GothamMedium' });
 
 $(document).ready(function(){
 
-	  gadgets.window.adjustHeight(660); //or whatever default height you please
+	  gadgets.window.adjustHeight(640); //or whatever default height you please
     //this will trigger the event handler below. Note that window in this case refers to the app iframe.
     //If you have width: auto or 100% it will also trigger whenever a user changes their browser window (width) size.
     $(window).resize(function(){
